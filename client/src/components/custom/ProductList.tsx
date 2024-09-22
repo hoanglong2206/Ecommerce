62244c5b71e475d2cd5632b00412b7543b9aeec7
hoanglong2206 <21522307@gm.uit.edu.vn>
2024-09-22 19:00:16 +0700

    fix

--- client/src/components/custom/ProductList.tsx
+++ client/src/components/custom/ProductList.tsx
@@ -1,9 +1,15 @@
 import { products } from "@/utils/products";
 import { ProductCard } from "@/components";
 
-const ProductList = () => {
+interface ProductListProps {
+  typeList: string;
+}
+
+const ProductList = ({ typeList }: ProductListProps) => {
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+    <div
+      className={`grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 ${typeList}`}
+    >
       {products.map((product, index) => (
         <div key={index} className="col-span-1">
           <ProductCard data={product} />
